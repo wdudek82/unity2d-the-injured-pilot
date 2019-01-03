030a98d1183a0c0bbd63fe2a4e42cd101d758116
Wojciech Dudek <wdudek82@gmail.com>
2019-01-03 18:41:20 +0100

    Initial

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+
+    [SerializeField] State startingState;
+
+    State state;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        for (int i = 0; i < nextStates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+                Debug.Log("Key pressed: " + KeyCode.Alpha1 + i);
+            }
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
+}
